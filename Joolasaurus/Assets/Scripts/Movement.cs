59965808ad58b8098af1b1175d48a6c02b289d64
F4ncyJackson <44419344+F4ncyJackson@users.noreply.github.com>
2018-10-23 23:09:56 -0500

    Update Joolasaurus/Assets/Scripts/Movement.cs

--- Joolasaurus/Assets/Scripts/Movement.cs
+++ Joolasaurus/Assets/Scripts/Movement.cs
@@ -20,7 +20,7 @@ public class Movement : MonoBehaviour {
 		float moveHorizontal = Input.GetAxis ("Horizontal");
 		float moveVertical = Input.GetAxis("Vertical");
 
-		Vector2 movement = new Vector2 (moveHorizontal, 0);
+		Vector2 movement = new Vector2 (moveHorizontal, moveVertical);
 		rb.AddForce (movement * speed);
 	}
 }
