c17692305904b52ac0e7cda1bab46a1d3c928bec
F4ncyJackson <44419344+F4ncyJackson@users.noreply.github.com>
2018-10-23 22:43:01 -0500

    I made death and movement hapen

---
+++ Joolasaurus/Assets/Scripts/Death.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Death : MonoBehaviour {
+
+	public GameObject player;
+
+	void OnTriggerEnter2D(Collider2D col)
+	{
+		Destroy (player);
+	}
+}
