906abe3547a5be648cb4a3d952880b5da47a2b4f
F4ncyJackson <44419344+F4ncyJackson@users.noreply.github.com>
2018-10-24 22:06:48 -0500

    Added Jumping

--- Joolasaurus/Assets/Scripts/Movement.cs
+++ Joolasaurus/Assets/Scripts/Movement.cs
@@ -5,9 +5,12 @@ using UnityEngine;
 public class Movement : MonoBehaviour {
 
 	private Rigidbody2D rb;
+	private bool shouldJump;
+	private bool canJump;
 
 
 	public float speed;
+	public float jumpPower;
 
 
 	// Use this for initialization
@@ -16,11 +19,36 @@ public class Movement : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
+	//movement
 	void Update () {
+
+		//walking/running
 		float moveHorizontal = Input.GetAxis ("Horizontal");
 		float moveVertical = Input.GetAxis("Vertical");
 
 		Vector2 movement = new Vector2 (moveHorizontal, moveVertical);
 		rb.AddForce (movement * speed);
+
+		//Jumping Key
+		if(Input.GetButtonDown("Jump"))
+		{
+			shouldJump = true;
+			canJump = false;
+		}	
+	}
+
+	void FixedUpdate()
+	{
+		if (shouldJump) 
+		{
+			//Jumping
+			rb.AddForce (Vector3.up * jumpPower, ForceMode2D.Impulse);
+			shouldJump = false; 
+		} 
+	}
+
+	private void OnCollisionEnter2D(Collision2D col)
+	{
+		canJump = true;
 	}
 }
